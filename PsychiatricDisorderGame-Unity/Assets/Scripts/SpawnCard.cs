1a8feaee0196160967b4be1b92e59d40303ba8bc
WilliamAkins <w.akins111@gmail.com>
2019-10-14 16:43:24 +0100

    got card spawning working with all different suits and numbers

---
+++ PsychiatricDisorderGame-Unity/Assets/Scripts/SpawnCard.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnCard : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject cardTemplate;
+
+    private GameObject currentCard;
+    
+    // Start is called before the first frame update
+    private void Start()
+    {
+        //Instantiate a blank template before any card data is assigned to it.
+        currentCard = Instantiate(cardTemplate, Vector3.zero, Quaternion.identity);
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        
+    }
+
+    private void pickCard()
+    {
+
+    }
+}
---
+++ PsychiatricDisorderGame-Unity/Assets/Scripts/cardInfo.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cardInfo : MonoBehaviour
+{
+    public int suitNum = 0;
+    public int cardNum = 0;
+
+    private string[] suit = new string[] { "clubs", "diamonds", "hearts", "spades" };
+
+    private void Start()
+    {
+        //randomly generate the string used to find the correct card image
+        suitNum = Random.Range(0, 4);
+        cardNum = Random.Range(1, 14);
+        string cardName = "card_" + suit[suitNum] + "_" + cardNum;
+
+        Debug.Log(cardName);
+
+        transform.GetChild(0).GetComponent<SpriteRenderer>().sprite = Resources.Load<Sprite>("card_faces/" + cardName);
+    }
+}
